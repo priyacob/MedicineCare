971d60df19dc6b577732059d8306b7090a2c8f16
rimamdt <rimadasmdt@gmail.com>
2025-02-23 09:32:19 +0530

    rima

--- navbar.php
+++ navbar.php
@@ -162,11 +162,11 @@
 						<div class="col-lg-2 col-12">
 							<?php if (isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN']) { ?>
 								<div class="get-quote">
-									<a href="logout.php" class="btn">Logout</a>
+									<a href="logout.php" class="btn" style="color:black;">Logout</a>
 								</div>
 							<?php } else { ?>
 								<div class="get-quote">
-									<a href="login.php" class="btn">SignUp/SignIn</a>
+									<a href="login.php" class="btn" style="color:black;">SignUp/SignIn</a>
 								</div>
 							<?php } ?>
 						</div>
