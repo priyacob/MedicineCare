98a47f4c003843a81f340492c8945487834df6db
Sayan <sayanbanikcob@gmail.com>
2025-02-24 14:25:13 +0530

    messagingThis

--- index.php
+++ index.php
@@ -1,75 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-
-$sql = "SELECT * FROM medicine"; // Replace with your actual table name
-$result = mysqli_query($con, $sql);
-?>
-
-	<!-- Slider Area -->
-	<section class="slider">
-		<div class="hero-slider">
-			<!-- Start Single Slider -->
-			<div class="single-slider" style="background-image:url('img/slider2.jpg')">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-7">
-							<div class="text">
-								<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl
-									pellentesque, faucibus libero eu, gravida quam. </p>
-								<div class="button">
-									<a href="#" class="btn">Get Appointment</a>
-									<a href="#" class="btn primary">Learn More</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- End Single Slider -->
-			<!-- Start Single Slider -->
-			<div class="single-slider" style="background-image:url('img/slider.jpg')">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-7">
-							<div class="text">
-								<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl
-									pellentesque, faucibus libero eu, gravida quam. </p>
-								<div class="button">
-									<a href="#" class="btn">Get Appointment</a>
-									<a href="#" class="btn primary">About Us</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- Start End Slider -->
-			<!-- Start Single Slider -->
-			<div class="single-slider" style="background-image:url('img/slider3.jpg')">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-7">
-							<div class="text">
-								<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl
-									pellentesque, faucibus libero eu, gravida quam. </p>
-								<div class="button">
-									<a href="#" class="btn">Get Appointment</a>
-									<a href="#" class="btn primary">Conatct Now</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- End Single Slider -->
-		</div>
-	</section>
-	<!--/ End Slider Area -->
-=======
 include('includes/db.php');
 include 'navbar.php';
 $sql = "SELECT * FROM medicine"; 
@@ -137,7 +67,6 @@ $result = mysqli_query($con, $sql);
 </section>
 <!--/ End Slider Area -->
 
->>>>>>> 021df8f0e38d93a50fc094ae441db3723309cd25
 <!-- cards -->
 
 
